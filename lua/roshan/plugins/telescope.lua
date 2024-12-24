e34f15fb7d9321480603fc111f936cd3c905b85c
Roshan Paudel <rozanpoudel@gmail.com>
2024-12-24 08:48:51 +0545

    fix: telescope mapping

--- lua/roshan/plugins/telescope.lua
+++ lua/roshan/plugins/telescope.lua
@@ -15,6 +15,8 @@ return {
         path_display = { "truncate " },
         mappings = {
           i = {
+            ["<S-Tab>"] = actions.move_selection_previous, -- move to prev result
+            ["<Tab>"] = actions.move_selection_next, -- move to next result
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
